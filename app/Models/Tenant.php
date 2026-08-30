48097bbde22564325894adbf31fbef379e90b595
Lailson das Virgens <lailsondev@gmail.com>
2026-08-30 20:49:44 -0300

    feat: model Tenant

---
+++ app/Models/Tenant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+
+class Tenant extends \Stancl\Tenancy\Database\Models\Tenant implements TenantWithDatabase
+{
+    Use HasDatabase, HasDomains;
+}
