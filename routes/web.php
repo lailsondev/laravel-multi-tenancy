0c80d893b6403dbf8fd096ad0c77e7fed9bee655
Lailson das Virgens <lailsondev@gmail.com>
2026-08-31 20:06:49 -0300

    tests: trecho de testes - será removido

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Tenant;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $tenant = Tenant::find('empresa1');
+
+
+    tenancy()->initialize($tenant);
+
+    \App\Models\Comment::all();
     return view('welcome');
 });
